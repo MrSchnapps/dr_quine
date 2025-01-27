b8bc1e703fdd992d8812e23aaf69eda3db1a725e
MrSchnapps <jules.decuypere@hotmail.fr>
2025-01-27 20:16:56 +0100

    big print sully

--- tmp/Sully.c
+++ tmp/Sully.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-void ft_print(FILE *file, char *str, int i)
-{
-    fprintf(file, str, 10, 34, str, i);
-}
+#include <string.h>
 
 int main()
 {
     int i = 5;
+    char filename_to_check[50];
+    sprintf(filename_to_check, "Sully%d_.c", i);
 
-    char filename[10];
-    filename[9] = '\0';
-    sprintf(filename, "Sully_%d.c", i);
-    FILE *file = fopen(filename, "w");
-    if (file == NULL) {
-        return (1);
-    }
+    if(!strcmp(filename_to_check, __FILE__))
+        i--;
     
-    //char *str = "#include <stdio.h>%1$c#include <stdlib.h>%1$cvoid ft_print(FILE *file, char *str, int i)%1$c{%1$c    fprintf(file, str, 10, 34, str, i);%1$c}%1$c%1$cint main()%1$c{%1$c    int i = %4$d;%1$c%1$c    i--;%1$c    char *filename = asprintf(%2$cSully_%%d.c%2$c, i);%1$c    FILE *file = fopen(filename, %2$cw%2$c);    %1$cif (file == NULL) {%1$c        return (1);%1$c    )%1$c    char * str = %2$c%3$s%2$c;%1$c%1$c    ft_print(file, str, i);%1$c    char *compileCmd = asprintf(%3$cclang -Wall -Wextra -Werror Sully_%1$d.c -0 Sully_%1$d ; ./Sully_%1$d%2$c, %1$d); }";
-    char *str = "#include <stdio.h>%1$cint main()%1$c{%1$c    int i = %3$d;%1$c    printf(%2$cnumero : %%d%2$c, i);%1$c    return(0);%1$c}";
-    fprintf(file, str, 10, 34, i);
-    fclose(file);
-    //ft_print(file, str, i);
+    if (i < 0)
+        return (0);
+
+    char new_filename[10];
+    new_filename[9] = '\0';
+    sprintf(new_filename, "Sully_%d.c", i);
+    FILE *file = fopen(new_filename, "w");
+    if (file == NULL)
+        return (1);
+    
+    char *str = "#include <stdio.h>%1$c#include <string.h>%1$c%1$cint main()%1$c{%1$c    int i = %3$d;%1$c    char filename_to_check[50];%1$c    sprintf(filename_to_check, %2$cSully%3$d_.c%2$c, i);%1$c%1$c    if(!strcmp(filename_to_check, __FILE__))}%1$c        i--;%1$c%1$cif (i < 0)%1$c        return (0);%1$c%1$c    char new_filename[10];%1$c    new_filename[9] = '\0';%1$c    sprintf(new_filename, %2$cSully%3$d_.c%2$c, i);%1$c    FILE *file = fopen(new_filename, %2$cw%2$c);%1$c    if (file == NULL)%1$c        return(1);%1$c%1$c char *str = %2$c%4$s%2$c;%1$c    fprintf(file, str, 10, 34, i, str);%1$c    if (fclose(file) != 0)%1$c        return(1);%1$c    char compileCmd[500];%1$c    sprintf(compileCmd, %2$cclang -Wall -Wextra -Werror Sully_%3$d.c -o Sully_%3$d ; ./Sully_%3$d%2$c, i);%1$c    system(compileCmd);%1$c    return(0);%1$c}";
+    fprintf(file, str, 10, 34, i, str);
+    if (fclose(file) != 0)
+        return (1);
     char compileCmd[500]; 
     sprintf(compileCmd, "clang -Wall -Wextra -Werror Sully_%1$d.c -o Sully_%1$d ; ./Sully_%1$d", i);
-    
     system(compileCmd);
     return (0);
 }
